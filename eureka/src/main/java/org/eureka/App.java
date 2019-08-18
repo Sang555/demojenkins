9b076fa31d7ee839b1b215050aa138951248c231
Sangeetha V S <sanvs@publicisgroupe.net>
2019-08-18 18:16:16 +0530

    first commit

---
+++ eureka/src/main/java/org/eureka/App.java
@@ -0,0 +1,19 @@
+package org.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
